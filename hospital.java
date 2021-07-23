c5214a64009d41515efa0f0c0f3d3493fef1bc26
Ashwini-Sriram <58700518+Ashwini-Sriram@users.noreply.github.com>
2021-07-23 23:03:55 +0530

    Add files via upload

---
+++ hospital.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ashwinisriram
+ */
+import java.util.*;
+
+abstract  class hospital {
+    String hospitalName;
+    int vaccine;//1 for covaxin and 2 for covishield 
+    int slots ;
+    
+    
+}
---
+++ person.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ashwinisriram
+ */
+import java.util.*;
+
+//implement interface
+ abstract class person {
+    String name;
+    int age;
+    String area;
+    int dosage;// 1 for 1st and 2 for 2nd
+    int phone;
+    
+    
+    
+}
